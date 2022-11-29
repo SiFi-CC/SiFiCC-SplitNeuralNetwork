de723d2aa265f190f0a63db42163e2982a907156
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-11-29 15:16:01 +0100

    initial commit

---
+++ env/source_conda_env_base.bash
@@ -0,0 +1,4 @@
+# source conda environment with all needed packages
+# only works on cluster node
+source /net/software_g4rt/anaconda3/etc/profile.d/conda.sh
+conda activate condaenv-sificcnn
---
+++ env/source_conda_env_gpu.bash
@@ -0,0 +1,4 @@
+# source conda environment for tensorflow gpu training
+# only works on GPU node
+source /net/software_g4rt/anaconda3/etc/profile.d/conda.sh
+conda activate condaenv-sificcnn-python39-tensorflowgpu24
