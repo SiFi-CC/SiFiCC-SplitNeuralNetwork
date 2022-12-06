80204448b45acf9ae2dc0a8f375a430b8f686ee6
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-12-06 11:36:06 +0100

    Update example_code.py

--- example_code.py
+++ example_code.py
@@ -13,10 +13,21 @@ dir_npz = dir_main + "/npz_files/"
 ########################################################################################################################
 
 # Reading root files and export it to npz files
-root1 = RootParser(root_files.OptimisedGeometry_BP0mm_2e10protons)
-root1.export_npz(dir_npz + "OptimisedGeometry_BP0mm_2e10protons.npz")
+root1 = RootParser(dir_main + root_files.OptimisedGeometry_BP0mm_2e10protons_offline)
+# root1.export_npz(dir_npz + "OptimisedGeometry_BP0mm_2e10protons.npz")
 
-root2 = RootParser(root_files.OptimisedGeometry_BP5mm_4e9protons)
-root2.export_npz(dir_npz + "OptimisedGeometry_BP5mm_4e9protons.npz")
+root2 = RootParser(dir_main + root_files.OptimisedGeometry_BP5mm_4e9protons_offline)
+# root2.export_npz(dir_npz + "OptimisedGeometry_BP5mm_4e9protons.npz")
+
+from classes import InputGeneratorDenseBase
+InputGeneratorDenseBase.gen_input(root1)
+InputGeneratorDenseBase.gen_input(root2)
+
+"""
+npz_data = np.load(dir_npz + "NNinputDenseBase.npz")
+ary_features = npz_data["features"]
+ary_targets = npz_data["targets"]
+ary_w = npz_data["weights"]
+"""
 
 
