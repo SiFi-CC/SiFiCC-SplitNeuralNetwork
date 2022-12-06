1d0e8b600493c18dc334035585d6302ea41b2871
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-12-06 14:41:02 +0100

    Update NPZParser.py

--- classes/NPZParser.py
+++ classes/NPZParser.py
@@ -1,9 +1,25 @@
-class NPZParser:
+import numpy as np
 
-    def __init__(self, npz_file):
-
-        self.npz_file = npz_file
+from classes import DataCluster
 
 
+def parse(npz_file):
+    """
 
-    
+    Args:
+         npz_file (): numpy compressed file containing dictionary entries for:
+                        - "META"
+                        - "features"
+                        - "targets"
+                        - "weights"
+
+    Return:
+        DataCluster object
+    """
+
+    ary_meta = npz_file["META"]
+    ary_features = npz_file["features"]
+    ary_targets = npz_file["targets"]
+    ary_weights = npz_file["weights"]
+
+    return DataCluster.DataCluster(ary_meta, ary_features, ary_targets, ary_weights)
