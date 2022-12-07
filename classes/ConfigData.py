38acc4490a149d8216d077cc95de54214162f2bd
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-08 00:02:43 +0100

    initial commit

--- classes/ConfigData.py
+++ classes/ConfigData.py
@@ -1,22 +1,24 @@
-class ConfigFileData:
+class ConfigData:
 
     def __init__(self,
                  root_file,
-                 npz_meta_file,
-                 npz_nninput_file,
                  input_generator,
                  training_strategy,
                  analysis,
+                 metadata,
+                 nninput,
+                 model,
                  epochs,
                  verbose,
                  batch_size):
 
         self.root_file = root_file
-        self.npz_meta_file = npz_meta_file
-        self.npz_nninput_file = npz_nninput_file
         self.input_generator = input_generator
         self.training_strategy = training_strategy
+        self.model = model
         self.analysis = analysis
+        self.metadata = metadata
+        self.nninput = nninput
 
         self.epochs = epochs
         self.batch_size = batch_size
