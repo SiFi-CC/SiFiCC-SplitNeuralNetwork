bfc810980984fc7cceeadcca71df887901669436
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-12-14 17:05:16 +0100

    added manual weighting plotting

--- analysis_sampleweights.py
+++ analysis_sampleweights.py
@@ -27,32 +27,27 @@ class_weights = [len(ary_meta[:, 2]) / (2 * counts[0]), len(ary_meta[:, 2]) / (2
 bins = np.concatenate([np.arange(0.0, max_e + 0.1, 0.1), [int(max(ary_mc[:, 0]))]])
 hist, _ = np.histogram(ary_mc[:, 0], bins=bins)
 
-fig, axs = plt.subplots(2, 1)
+# manual energy weights
+bins2 = np.array([0.0, 2.5, 4.3, 4.5, 8.0, max_e + 2.0])
+hist2 = np.array([0.1, 1.0, 5.0, 3.0, 5.0])
+
+fig, axs = plt.subplots(3, 1)
 axs[0].set_xlim(0.0, max_e + 1.0)
-axs[0].hist(ary_mc[:, 0], bins=bins, histtype=u"step", color="black", label="MCEnergyPrimary")
-axs[0].legend()
-# axs[1].hlines(xmin=0.0, xmax=21.0, y=len(ary_mc[:, 0]) / (len(bins) - 1), color="blue", linestyles="--")
-axs[1].set_xlim(0.0, max_e + 1.0)
-axs[1].plot(bins[:-1] + 0.05, len(ary_mc[:, 0]) / (len(bins) - 1) / hist, color="blue", label="weights")
-axs[1].legend()
+axs[0].set_xticks([])
+axs[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 axs[0].set_ylabel("Total counts")
 axs[1].set_ylabel("weight")
-axs[1].set_xlabel("MC Primary Energy [MeV]")
+axs[1].set_xlim(0.0, max_e + 1.0)
+axs[1].set_xticks([])
+axs[2].set_xlim(0.0, max_e + 1.0)
+axs[2].set_ylabel("weight")
+axs[2].set_xlabel("MC Primary Energy [MeV]")
+axs[0].hist(ary_mc[:, 0], bins=bins, histtype=u"step", color="black", label="MCEnergyPrimary")
+axs[0].legend()
+axs[1].plot(bins[:-1] + 0.05, len(ary_mc[:, 0]) / (len(bins) - 1) / hist, color="blue", label="bin weights")
+axs[1].legend()
+axs[2].stairs(hist2, bins2, color="red", label="pre-defined weights")
+axs[2].legend()
+plt.tight_layout()
 plt.show()
 
-print("\nClass weights")
-print(class_weights)
-print("\nEnergy weights")
-print(ary_meta.shape[0] / (len(bins) - 1) / hist)
-
-ary_w = np.zeros(shape=(ary_meta.shape[0],))
-ary_targets = ary_meta[:, 2]
-for i in range(len(ary_w)):
-    for j in range(len(bins) - 1):
-        if bins[j] < ary_mc[i, 0] < bins[j + 1]:
-            energy_weight = len(ary_targets) / (len(bins) - 1) / hist[j]
-            ary_w[i] = energy_weight * class_weights[int(ary_targets[i])]
-            break
-
-for i in range(20):
-    print(ary_mc[i, 0], ary_w[i])
