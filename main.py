bd1f9e2ef76637942d7ad69b14de20e5269587e1
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-15 20:57:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,34 @@ from classes import MetaData
 
 import sys
 
+
 ########################################################################################################################
-"""
-DataCluster = NPZParser.parse(config_data.nninput)
-meta_data = MetaData.MetaData(config_data.metadata)
 
-# evaluate model expression
-model_method = __import__("models." + config_data.model, fromlist=[None])
-func1 = getattr(model_method, "return_model")
-model = func1(DataCluster.num_features())
+def execute(config_data):
+    DataCluster = NPZParser.parse(config_data.nninput)
+    meta_data = MetaData.MetaData(config_data.metadata)
 
-neuralnetwork = SiFiCCNNTF.SiFiCCNNTF(model=model,
-                                      model_name=config_data.model,
-                                      model_tag=config_data.modeltag)
+    # evaluate model expression
+    model_method = __import__("models." + config_data.model, fromlist=[None])
+    func1 = getattr(model_method, "return_model")
+    model = func1(DataCluster.num_features())
 
-# evaluate training strategy expression
-trainingstrategy_method = __import__("trainingstrategy." + config_data.training_strategy, fromlist=[None])
-func2 = getattr(trainingstrategy_method, "train_strategy")
-func2(neuralnetwork, DataCluster)
+    neuralnetwork = SiFiCCNNTF.SiFiCCNNTF(model=model,
+                                          model_name=config_data.model,
+                                          model_tag=config_data.modeltag)
 
-# evaluate analysis expression
-# analysis expressions can be a list of analysis methods
-for i in range(len(config_data.analysis)):
-    analysis_method = __import__("analysis." + config_data.analysis[i], fromlist=[None])
-    analysis = getattr(analysis_method, "analysis")
-    analysis(neuralnetwork, DataCluster, meta_data)
-"""
+    # evaluate training strategy expression
+    trainingstrategy_method = __import__("trainingstrategy." + config_data.training_strategy, fromlist=[None])
+    func2 = getattr(trainingstrategy_method, "train_strategy")
+    func2(neuralnetwork, DataCluster)
+
+    # evaluate analysis expression
+    # analysis expressions can be a list of analysis methods
+    for i in range(len(config_data.analysis)):
+        analysis_method = __import__("analysis." + config_data.analysis[i], fromlist=[None])
+        analysis = getattr(analysis_method, "analysis")
+        analysis(neuralnetwork, DataCluster, meta_data)
+
 
 ########################################################################################################################
 
@@ -46,8 +48,7 @@ def main():
         if args.geninput:
             print("Generating input")
 
-        print("nothing else!")
-        sys.exit()
+        execute(config_data)
 
     else:
         print("Error found in configfile!")
