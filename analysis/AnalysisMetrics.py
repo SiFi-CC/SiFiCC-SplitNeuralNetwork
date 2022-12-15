09efffd2572ecd096ff7ca41b4a4f2dac2867f87
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-15 20:53:04 +0100

    changed metric evaluation

--- analysis/AnalysisMetrics.py
+++ analysis/AnalysisMetrics.py
@@ -1,63 +1,97 @@
-def analysis(SiFiCCNN, DataCluster, MetaData=None):
-    import os
-    from fastROCAUC import fastROCAUC
+def metrics(y_scores, y_true, threshold, weighted=False):
+    import numpy as np
 
-    # predict test set
-    y_pred = SiFiCCNN.predict(DataCluster.x_test())
-    y_true = DataCluster.y_test()
+    # pre-define
+    y_pred = np.zeros(shape=(len(y_true, )))
 
-    # run ROC curve and AUC score analysis
-    auc, theta = fastROCAUC(y_pred, y_true, return_score=True)
-
-    # best optimal threshold
-    threshold = theta
-
-    # evaluate important metrics
-    TP = 0
-    FP = 0
-    TN = 0
-    FN = 0
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
 
     for i in range(len(y_pred)):
         # apply prediction threshold
-        if y_pred[i] >= threshold:
+        if y_scores[i] >= threshold:
             y_pred[i] = 1
         else:
             y_pred[i] = 0
 
         if y_pred[i] == 1 and y_true[i] == 1:
-            TP += 1
+            tp += 1
         if y_pred[i] == 1 and y_true[i] == 0:
-            FP += 1
+            fp += 1
         if y_pred[i] == 0 and y_true[i] == 0:
-            TN += 1
+            tn += 1
         if y_pred[i] == 0 and y_true[i] == 1:
-            FN += 1
+            fn += 1
 
-    if (TP + FN) == 0:
+    if (tp + fn) == 0:
         efficiency = 0
     else:
-        efficiency = TP / (TP + FN)
-    if (TP + FP) == 0:
+        efficiency = tp / (tp + fn)
+    if (tp + fp) == 0:
         purity = 0
     else:
-        purity = TP / (TP + FP)
+        purity = tp / (tp + fp)
 
-    ### TEMPRORARY
-    # grab non identified ideal compton event event-numbers
-    list_FN = []
-    ary_eventnumber = MetaData.event_number()[DataCluster.idx_test()]
-    for i in range(len(ary_eventnumber)):
-        if y_pred[i] == 0 and y_true[i] == 1:
-            list_FN.append(ary_eventnumber[i])
+    if weighted:
+        # set sample weights to class weights
+        _, counts = np.unique(y_pred, return_counts=True)
+        class_weights = [len(y_pred) / (2 * counts[0]), len(y_pred) / (2 * counts[1])]
 
-    print("\nClassification results: ")
-    print("AUC score: {:.3f}".format(auc))
+        accuracy = ((tp * class_weights[1]) + (tn * class_weights[0])) / (
+                    ((tp + fp) * class_weights[1]) + ((tn + fn) * class_weights[0]))
+    else:
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+
+    return accuracy, efficiency, purity, (tp, fp, tn, fn)
+
+
+def analysis(SiFiCCNN, DataCluster, MetaData=None):
+    import os
+    from fastROCAUC import fastROCAUC
+
+    ####################################################################################################################
+    # content of AnalysisMetric:
+    #
+    # Baseline accuracy evaluation
+    # weighted accuracy evaluation (historical since keras uses weighted accuracy)
+    # optimal threshold accuracy evaluation
+    # score distribution
+    #
+    # summary saved in txt file
+    ####################################################################################################################
+
+    # predict test set
+    y_scores = SiFiCCNN.predict(DataCluster.x_test())
+    y_true = DataCluster.y_test()
+
+    acc_base, eff_base, pur_base, conf_base = metrics(y_scores, y_true, threshold=0.5)
+    print("\nMetrics base threshold: ")
+    print("Threshold: {:.3f}".format(0.5))
+    print("Accuracy: {:.1f}".format(acc_base * 100))
+    print("Efficiency: {:.1f}%".format(eff_base * 100))
+    print("Purity: {:.1f}%".format(pur_base * 100))
+    print("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_base))
+
+    acc_weight, eff_weight, pur_weight, conf_weight = metrics(y_scores, y_true, threshold=0.5, weighted=True)
+    print("\nMetrics weighted threshold: ")
+    print("Threshold: {:.3f}".format(0.5))
+    print("Accuracy: {:.1f}".format(acc_weight * 100))
+    print("Efficiency: {:.1f}%".format(eff_weight * 100))
+    print("Purity: {:.1f}%".format(pur_weight * 100))
+    print("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_weight))
+
+    # run ROC curve and AUC score analysis
+    auc, theta = fastROCAUC(y_scores, y_true, return_score=True)
+    acc_opt, eff_opt, pur_opt, conf_opt = metrics(y_scores, y_true, threshold=theta)
+    print("\nMetrics base threshold: ")
+    print("AUC Score: {:.3f}".format(auc))
     print("Threshold: {:.3f}".format(theta))
-    print("Accuracy: {:.1f}".format((TP + TN) / (TP + TN + FP + FN) * 100))
-    print("Efficiency: {:.1f}%".format(efficiency * 100))
-    print("Purity: {:.1f}%".format(purity * 100))
-    print("TP: {} | TN: {} | FP: {} | FN: {}".format(TP, TN, FP, FN))
+    print("Accuracy: {:.1f}".format(acc_opt * 100))
+    print("Efficiency: {:.1f}%".format(eff_opt * 100))
+    print("Purity: {:.1f}%".format(pur_opt * 100))
+    print("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_opt))
 
     # save results in txt file
     dir_main = os.getcwd()
@@ -67,12 +101,25 @@ def analysis(SiFiCCNN, DataCluster, MetaData=None):
 
     with open(dir_results + "/metrics.txt", 'w') as f:
         f.write("### AnalysisMetric results:\n")
-        f.write("AUC score: {:.3f}\n".format(auc))
-        f.write("Threshold: {:.3f}\n".format(theta))
-        f.write("Accuracy: {:.1f}\n".format((TP + TN) / (TP + TN + FP + FN) * 100))
-        f.write("Efficiency: {:.1f}%\n".format(efficiency * 100))
-        f.write("Purity: {:.1f}%\n".format(purity * 100))
-        f.write("TP: {} | TN: {} | FP: {} | FN: {}\n".format(TP, TN, FP, FN))
-        f.write("\n")
-        for i in range(20):
-            f.write(str(list_FN[i]) + "\n")
+        f.write("\nMetrics base threshold: ")
+        f.write("Threshold: {:.3f}".format(0.5))
+        f.write("Accuracy: {:.1f}".format(acc_base * 100))
+        f.write("Efficiency: {:.1f}%".format(eff_base * 100))
+        f.write("Purity: {:.1f}%".format(pur_base * 100))
+        f.write("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_base))
+
+        f.write("\nMetrics weighted threshold: ")
+        f.write("Threshold: {:.3f}".format(0.5))
+        f.write("Accuracy: {:.1f}".format(acc_weight * 100))
+        f.write("Efficiency: {:.1f}%".format(eff_weight * 100))
+        f.write("Purity: {:.1f}%".format(pur_weight * 100))
+        f.write("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_weight))
+
+        f.write("\nMetrics base threshold: ")
+        f.write("AUC Score: {:.3f}".format(auc))
+        f.write("Threshold: {:.3f}".format(theta))
+        f.write("Accuracy: {:.1f}".format(acc_opt * 100))
+        f.write("Efficiency: {:.1f}%".format(eff_opt * 100))
+        f.write("Purity: {:.1f}%".format(pur_opt * 100))
+        f.write("TP: {} | FP: {} | TN: {} | FN: {}".format(*conf_opt))
+        
