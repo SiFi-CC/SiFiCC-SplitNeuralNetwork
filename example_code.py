7c92c1082387c9ba5a70bd6d002943c6025674d6
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-19 18:01:00 +0100

    update settings

--- example_code.py
+++ example_code.py
@@ -20,35 +20,3 @@ root1 = RootParser(dir_main + root_files.OptimisedGeometry_BP0mm_2e10protons_off
 root2 = RootParser(dir_main + root_files.OptimisedGeometry_BP5mm_4e9protons_offline)
 # root2.export_npz(dir_npz + "OptimisedGeometry_BP5mm_4e9protons.npz")
 
-"""
-from inputgenerator.InputGeneratorDenseRelative import gen_input
-gen_input(root1)
-gen_input(root2)
-
-npz_data = np.load(dir_npz + "NNInputDenseRelative_OptimisedGeometry_BP0mm_2e10protons.npz")
-ary_meta = npz_data["features"]
-print(ary_meta)
-"""
-
-"""
-from scripts import event_display
-
-counter = 0
-while counter < 10:
-    i = np.random.randint(0, root1.events.numentries)
-    event = root1.get_event(i)
-
-    # conditions
-    if event.is_ideal_compton:
-        continue
-
-    if event.MCEnergy_Primary < 5.0:
-        continue
-
-    event_display.event_display(root1, n=i)
-    counter += 1
-"""
-
-from inputgenerator.InputGeneratorDenseDistributed import gen_input
-gen_input(root1)
-
