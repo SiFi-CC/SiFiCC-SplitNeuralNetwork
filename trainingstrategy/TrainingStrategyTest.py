30ad2da984fd480511e6823055f19f9aedd744d2
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-19 18:02:43 +0100

    update loading method

--- trainingstrategy/TrainingStrategyTest.py
+++ trainingstrategy/TrainingStrategyTest.py
@@ -1,24 +1,27 @@
-def train_strategy(SiFiCCNN, DataCluster):
+def train_strategy(SiFiCCNN, DataCluster, load_model=False):
     # update training-valid ration to increase validation set
-    DataCluster.p_train = 0.6
-    DataCluster.p_valid = 0.2
+    DataCluster.p_train = 0.7
+    DataCluster.p_valid = 0.1
 
     # set class weights as sample weights
-    # DataCluster.weights *= DataCluster.get_classweights()
+    DataCluster.weights *= DataCluster.get_classweights()
+    DataCluster.weights *= DataCluster.get_energyweights()
 
     # standardize input
-    # DataCluster.normalize_by_eprimary()
     DataCluster.standardize()
 
     SiFiCCNN.epochs = 20
     SiFiCCNN.batch_size = 256
 
-    # train model
-    SiFiCCNN.train(DataCluster.x_train(),
-                   DataCluster.y_train(),
-                   DataCluster.w_train(),
-                   DataCluster.x_valid(),
-                   DataCluster.y_valid())
+    if not load_model:
+        # train model
+        SiFiCCNN.train(DataCluster.x_train(),
+                       DataCluster.y_train(),
+                       DataCluster.w_train(),
+                       DataCluster.x_valid(),
+                       DataCluster.y_valid())
 
-    # save model
-    SiFiCCNN.save()
+        # save model
+        SiFiCCNN.save()
+    else:
+        SiFiCCNN.load()
