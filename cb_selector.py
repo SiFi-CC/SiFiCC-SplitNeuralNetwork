20d5838b7e3db7619f082a3d17ffa6e059f0a910
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-12-22 08:37:53 +0100

    initial commit

---
+++ cb_selector.py
@@ -0,0 +1,49 @@
+import numpy as np
+import os
+
+from classes import RootParser
+from classes import root_files
+
+
+def check_compton_kinematics(event):
+    ELECTRON_MASS = 0.511
+    event_energy = np.sum(event.RecoClusterEnergies_values)
+    event_energy_uncertainty = np.sqrt(np.sum(event.RecoClusterEnergies_values ** 2))
+    compton_edge = ((event_energy / (1 + ELECTRON_MASS / (2 * event_energy))),
+                    event_energy * (ELECTRON_MASS + event_energy) / (ELECTRON_MASS / 2 + event_energy) / (
+                                ELECTRON_MASS / 2 + event_energy) * event_energy_uncertainty)
+
+
+
+
+def main():
+    dir_main = os.getcwd()
+    dir_root = dir_main + "/root_files/"
+
+    root_data = RootParser(dir_main + root_files.OptimisedGeometry_BP0mm_2e10protons_offline)
+
+    n = 1000
+    positives = 0.0
+
+    for i, event in enumerate(root_data.iterate_events(n=n)):
+        cb_reco_tag = 1
+        cb_true_tag = event.Identified
+
+        # sort cluster by index
+        idx_scatterer, idx_absorber = event.sort_clusters_by_module(use_energy=True)
+
+        # check scatterer cluster multiplicity
+        if not len(idx_scatterer) == 1:
+            cb_reco_tag = 0
+
+        # check
+
+        if cb_reco_tag == 1.0 and cb_true_tag != 0.0:
+            positives += 1.0
+        print(cb_reco_tag, abs(cb_true_tag))
+
+    print("Positive Rate: {:.1f}%".format(positives / n * 100))
+
+
+if __name__ == "__main__":
+    main()
