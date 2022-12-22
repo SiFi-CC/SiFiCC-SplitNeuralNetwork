7aa1ab521015efd5fca99d112847cee20809b29f
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-12-22 09:33:27 +0100

    Update cb_selector.py

--- cb_selector.py
+++ cb_selector.py
@@ -6,14 +6,18 @@ from classes import root_files
 
 
 def check_compton_kinematics(event):
+    # TODO: check correctness
     ELECTRON_MASS = 0.511
     event_energy = np.sum(event.RecoClusterEnergies_values)
     event_energy_uncertainty = np.sqrt(np.sum(event.RecoClusterEnergies_values ** 2))
     compton_edge = ((event_energy / (1 + ELECTRON_MASS / (2 * event_energy))),
                     event_energy * (ELECTRON_MASS + event_energy) / (ELECTRON_MASS / 2 + event_energy) / (
-                                ELECTRON_MASS / 2 + event_energy) * event_energy_uncertainty)
-
-
+                            ELECTRON_MASS / 2 + event_energy) * event_energy_uncertainty)
+    electron_energy_value, electron_energy_uncertainty = event.get_electron_energy()
+    # print(electron_energy_value - electron_energy_uncertainty, compton_edge[0] + compton_edge[1])
+    if electron_energy_value - electron_energy_uncertainty > compton_edge[0] + compton_edge[1]:
+        return False
+    return True
 
 
 def main():
@@ -36,11 +40,13 @@ def main():
         if not len(idx_scatterer) == 1:
             cb_reco_tag = 0
 
-        # check
+        if not check_compton_kinematics(event):
+            cb_reco_tag = 0
 
         if cb_reco_tag == 1.0 and cb_true_tag != 0.0:
             positives += 1.0
-        print(cb_reco_tag, abs(cb_true_tag))
+        if cb_reco_tag == 0.0 and cb_true_tag == 0.0:
+            positives += 1.0
 
     print("Positive Rate: {:.1f}%".format(positives / n * 100))
 
