9c1357cdb6f41570c5f928cf25b651a71067e8ed
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-01-03 21:10:06 +0100

    updated  dnn models

--- models/DNN_base_classifier.py
+++ models/DNN_base_classifier.py
@@ -13,13 +13,10 @@ def return_model(input_dim):
     layers = keras.layers
 
     ####################################################################################################################
-    # model settings
-    dense_nodes = 128
-    dense_layers = 4
 
     activation = "relu"
     loss_function = "binary_crossentropy"
-    dropout_rate = 0.2
+    dropout_rate = 0.20
     lr = 1e-3
 
     ####################################################################################################################
@@ -28,12 +25,12 @@ def return_model(input_dim):
     model = keras.models.Sequential()
     # add first dense layer with predefined input dimension
     model.add(tf.keras.layers.Dense(64, input_dim=input_dim, activation=activation))
-    model.add(tf.keras.layers.Dense(16, activation=activation))
-    model.add(tf.keras.layers.Dense(8, activation=activation))
+    model.add(tf.keras.layers.Dense(32, activation=activation))
     model.add(tf.keras.layers.Dropout(dropout_rate))
     # output node with sigmoid as activation function
     model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
     # compile model with loss function, optimizer and accuracy
     model.compile(loss=loss_function, optimizer=tf.keras.optimizers.Adam(learning_rate=lr), metrics=["accuracy"])
+    print(model.summary)
     # return model
     return model
---
+++ models/DNN_base_regression_energy.py
@@ -0,0 +1,26 @@
+def return_model(input_dim):
+    """
+    Dense neural network with basic structure.
+    Set to be a baseline model for future neural networks.
+
+    Args:
+        input_dim (int): dimension of the features (number of features)
+
+    :return: Tensorflow model
+    """
+    import tensorflow as tf
+    from tensorflow import keras
+    layers = keras.layers
+
+    ####################################################################################################################
+
+    model = keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(256, input_dim=input_dim, activation="relu"))
+    model.add(tf.keras.layers.Dense(256, activation="relu"))
+    model.add(tf.keras.layers.Dense(128, activation="relu"))
+    model.add(tf.keras.layers.Dense(64, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.1))
+    model.add(tf.keras.layers.Dense(2, activation="relu"))
+    model.compile(loss="mean_absolute_error", optimizer="adam", metrics=["mean_absolute_error"])
+
+    return model
