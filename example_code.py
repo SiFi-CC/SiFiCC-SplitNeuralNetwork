9cc189a8e4c3a6df0ab29abc9c50c5e4480d81e7
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-01-30 22:37:01 +0100

    added example solution for new standization

--- example_code.py
+++ example_code.py
@@ -21,6 +21,30 @@ root1 = RootParser(dir_main + root_files.OptimisedGeometry_BP0mm_2e10protons_off
 root2 = RootParser(dir_main + root_files.OptimisedGeometry_BP5mm_4e9protons_offline)
 # root2.export_npz(dir_npz + "OptimisedGeometry_BP5mm_4e9protons.npz")
 
-from inputgenerator import InputGeneratorDNN_dist_Mixed
+npz_data = np.load(dir_npz + "OptimisedGeometry_BP0mm_2e10protons_DNN_Base.npz")
+features = npz_data["features"]
 
-InputGeneratorDNN_dist_Mixed.gen_input([root1, root2])
+n_cluster = 8
+n_features = 9
+list_mean = []
+list_std = []
+list_idx = np.arange(0, n_cluster * n_features, n_features)
+for i in range(9):
+    # print(np.array(list_idx) + i)
+    ary_con = np.reshape(features[:, np.array(list_idx) + i], (features.shape[0]*n_cluster,))
+    list_mean.append(np.mean(ary_con))
+    list_std.append(np.std(ary_con))
+    print(list_mean[i], list_std[i])
+
+list_mean = list_mean * n_cluster
+list_std = list_std * n_cluster
+
+for i in range(len(list_mean)):
+    mean = list_mean[i]
+    std = list_std[i]
+    features[:, i] -= mean
+    features[:, i] /= std
+
+x_feat = features[6, :]
+x_feat = np.reshape(x_feat, (8, 9))
+print(x_feat)
