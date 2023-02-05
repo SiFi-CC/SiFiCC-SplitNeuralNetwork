4ca2c0f32686a9857c9351b6e87b0ad974fd8afe
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-02-05 23:21:02 +0100

    move classifier metric from EvaluationHandler

--- src/Metrics.py
+++ src/Metrics.py
@@ -68,3 +68,51 @@ def get_global_effpur(n_positive_total,
     purity = n_correct / np.sum(ary_class_true)
 
     return efficiency, purity
+
+
+
+def get_classifier_metrics(y_scores, y_true, threshold, weighted=False):
+    # pre-define
+    y_pred = np.zeros(shape=(len(y_true, )))
+
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+
+    for i in range(len(y_pred)):
+        # apply prediction threshold
+        if y_scores[i] >= threshold:
+            y_pred[i] = 1
+        else:
+            y_pred[i] = 0
+
+        if y_pred[i] == 1 and y_true[i] == 1:
+            tp += 1
+        if y_pred[i] == 1 and y_true[i] == 0:
+            fp += 1
+        if y_pred[i] == 0 and y_true[i] == 0:
+            tn += 1
+        if y_pred[i] == 0 and y_true[i] == 1:
+            fn += 1
+
+    if (tp + fn) == 0:
+        efficiency = 0
+    else:
+        efficiency = tp / (tp + fn)
+    if (tp + fp) == 0:
+        purity = 0
+    else:
+        purity = tp / (tp + fp)
+
+    if weighted:
+        # set sample weights to class weights
+        _, counts = np.unique(y_pred, return_counts=True)
+        class_weights = [len(y_pred) / (2 * counts[0]), len(y_pred) / (2 * counts[1])]
+
+        accuracy = ((tp * class_weights[1]) + (tn * class_weights[0])) / (
+                ((tp + fp) * class_weights[1]) + ((tn + fn) * class_weights[0]))
+    else:
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+
+    return accuracy, efficiency, purity, (tp, fp, tn, fn)
