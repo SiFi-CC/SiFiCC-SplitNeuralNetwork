ad6a2954c3c74cefb693d6a5064cbe80af9b62c9
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-02-05 23:22:35 +0100

    Update DNN_base_regression_position.py

--- models/DNN_base_regression_position.py
+++ models/DNN_base_regression_position.py
@@ -13,13 +13,26 @@ def return_model(input_dim):
     layers = keras.layers
 
     ####################################################################################################################
+    """
+    # create model
+    model = keras.models.Sequential()
+    # add first dense layer with predefined input dimension
+    model.add(tf.keras.layers.Dense(300, input_dim=input_dim, activation="relu"))
+    model.add(tf.keras.layers.Dense(300, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.30))
+    model.add(tf.keras.layers.Dense(100, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.20))
+    model.add(tf.keras.layers.Dense(6, activation="linear"))
+    model.compile(loss="mean_absolute_error", optimizer="SGD", metrics=["mean_absolute_error"])
+    # return model
+    return model
+    """
 
     # create model
     model = keras.models.Sequential()
     # add first dense layer with predefined input dimension
     model.add(tf.keras.layers.Dense(64, input_dim=input_dim, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.2))
-    model.add(tf.keras.layers.Dense(16, activation="relu"))
+    # model.add(tf.keras.layers.Dropout(0.05))
     model.add(tf.keras.layers.Dense(6, activation="linear"))
     model.compile(loss="mean_absolute_error", optimizer="SGD", metrics=["mean_absolute_error"])
     # return model
