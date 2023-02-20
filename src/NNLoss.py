0a2c619ebf9813b0901b5640c9a4da225c214c8b
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-02-20 22:02:59 +0100

    added asymmetrical loss

--- src/NNLoss.py
+++ src/NNLoss.py
@@ -18,3 +18,22 @@ def loss_energy_mse_relative(y_pred, y_true, batch_size=32):
         loss += ((y_true[i, 0] - y_pred[i, 0]) ** 2 / y_true[i, 0] +
                  (y_true[i, 1] - y_pred[i, 1]) ** 2 / y_true[i, 1]) / 2
     return loss / n
+
+
+def loss_energy_mae_asym(y_pred, y_true, batch_size=32):
+    n = len(y_pred)
+    a = 10
+    loss = 0.0
+
+    for i in range(n):
+        loss1 = (y_true[i, 0] - y_pred[i, 0]) / y_true[i, 0]
+        loss2 = (y_true[i, 1] - y_pred[i, 1]) / y_true[i, 1]
+
+        if loss1 < 0.0:
+            loss1 *= a
+        if loss2 < 0.0:
+            loss2 *= a
+
+        loss += (abs(loss1) + abs(loss2)) / 2
+
+    return loss / n
