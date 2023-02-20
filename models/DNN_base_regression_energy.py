29ec4987e3621d142d3043c79f39d2fb812968fd
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-02-20 22:04:17 +0100

    Update DNN_base_regression_energy.py

--- models/DNN_base_regression_energy.py
+++ models/DNN_base_regression_energy.py
@@ -28,13 +28,25 @@ def return_model(input_dim):
     """
 
     ####################################################################################################################
+    """   
     # create model
     model = keras.models.Sequential()
     # add first dense layer with predefined input dimension
     model.add(tf.keras.layers.Dense(128, input_dim=input_dim, activation="relu"))
     model.add(tf.keras.layers.Dense(64, activation="relu"))
     model.add(tf.keras.layers.Dense(32, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.1))
+    # model.add(tf.keras.layers.Dropout(0.1))
     model.add(tf.keras.layers.Dense(2, activation="relu"))
-    model.compile(loss=loss_energy_relative, optimizer="SGD", metrics=["mean_absolute_error"])
+    model.compile(loss="mean_absolute_error", optimizer="SGD", metrics=["mean_absolute_error"])
+    return model
+    """
+
+    # create model
+    model = keras.models.Sequential()
+    # add first dense layer with predefined input dimension
+    model.add(tf.keras.layers.Dense(128, input_dim=input_dim, activation="relu"))
+    model.add(tf.keras.layers.Dense(32, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.1))
+    model.add(tf.keras.layers.Dense(2, activation="linear"))
+    model.compile(loss=loss_energy_asym, optimizer="Adam", metrics=["mean_absolute_error"])
     return model
