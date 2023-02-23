e455d9e73142d20183e2f9422627c59461b3f340
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2023-02-23 15:01:26 +0100

    Update NNAnalysis.py

--- src/NNAnalysis.py
+++ src/NNAnalysis.py
@@ -28,22 +28,22 @@ def regression_nn_vs_cb(ary_nn_pred, ary_cb_reco, ary_mc_truth, ary_meta, theta=
                                        ary_mc_truth[idx_ic, 2],
                                        ary_nn_pred[idx_pos, 3] - ary_mc_truth[idx_pos, 2],
                                        ary_cb_reco[idx_idenified, 2] - ary_mc_truth[idx_idenified, 2],
-                                       "electron",
+                                       "position_nn_vs_cb",
                                        "x",
-                                       "position_nn_vs_cb")
+                                       "electron",)
     Plotter.plot_reg_nn_vs_cb_position(ary_nn_pred[idx_pos, 4],
                                        ary_cb_reco[idx_idenified, 3],
                                        ary_mc_truth[idx_ic, 3],
                                        ary_nn_pred[idx_pos, 4] - ary_mc_truth[idx_pos, 3],
                                        ary_cb_reco[idx_idenified, 3] - ary_mc_truth[idx_idenified, 3],
-                                       "electron",
+                                       "position_nn_vs_cb",
                                        "y",
-                                       "position_nn_vs_cb")
+                                       "electron",)
     Plotter.plot_reg_nn_vs_cb_position(ary_nn_pred[idx_pos, 5],
                                        ary_cb_reco[idx_idenified, 4],
                                        ary_mc_truth[idx_ic, 4],
                                        ary_nn_pred[idx_pos, 5] - ary_mc_truth[idx_pos, 4],
                                        ary_cb_reco[idx_idenified, 4] - ary_mc_truth[idx_idenified, 4],
-                                       "electron",
+                                       "position_nn_vs_cb",
                                        "z",
-                                       "position_nn_vs_cb")
+                                       "electron",)
