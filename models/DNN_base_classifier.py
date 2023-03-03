9d4ea702820b980431cb4fd76507f4f18aeb2d17
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-03-03 16:43:23 +0100

    updated classifier model

--- models/DNN_base_classifier.py
+++ models/DNN_base_classifier.py
@@ -17,15 +17,15 @@ def return_model(input_dim):
     # create model
     model = keras.models.Sequential()
     # add first dense layer with predefined input dimension
-    model.add(tf.keras.layers.Dense(256, input_dim=input_dim, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.1))
-    model.add(tf.keras.layers.Dense(128, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.1))
-    model.add(tf.keras.layers.Dense(16, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.1))
+    model.add(tf.keras.layers.Dense(64, input_dim=input_dim, activation="relu"))
+    model.add(tf.keras.layers.Dense(64, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(32, activation="relu"))
+    model.add(tf.keras.layers.Dense(32, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
     # compile model with loss function, optimizer and accuracy
-    model.compile(loss="binary_crossentropy", optimizer="SGD",
+    model.compile(loss="binary_crossentropy", optimizer="Adam",
                   metrics=[keras.metrics.Precision(), keras.metrics.Recall()])
     # return model
     return model
