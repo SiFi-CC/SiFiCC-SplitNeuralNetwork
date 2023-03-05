690bba7f13d1f06e0fbdf63e3c2873a97c695f6e
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-03-05 22:09:28 +0100

    Create script_Toyset_analysis.py

---
+++ script_Toyset_analysis.py
@@ -0,0 +1,64 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from src import Plotter
+from src import ToyGenerator
+
+plt.rcParams.update({'font.size': 14})
+
+dir_main = os.getcwd()
+dir_root = dir_main + "/root_files/"
+dir_npz = dir_main + "/npz_files/"
+dir_toy = dir_main + "/toy/"
+dir_results = dir_main + "/results/"
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Toy set generation
+"""
+# Base sample
+ToyGenerator.create_toy_set(FILE_NAME="S1AX_continuous_master",
+                            TAG="base",
+                            PATH_NN_PRED_0MM=dir_results + "DNN_S1AX_continuous_master/OptimisedGeometry_BP0mm_2e10protons_withTimestamps_DNN_S1AX/OptimisedGeometry_BP0mm_2e10protons_withTimestamps_DNN_S1AX.npz",
+                            PATH_NN_PRED_5MM=dir_results + "DNN_S1AX_continuous_master/OptimisedGeometry_BP5mm_4e9protons_withTimestamps_DNN_S1AX/OptimisedGeometry_BP5mm_4e9protons_withTimestamps_DNN_S1AX.npz",
+                            PATH_MC_TRUTH_0MM=dir_npz + "OptimisedGeometry_BP0mm_2e10protons_withTimestamps_S1AX_lookup.npz",
+                            PATH_MC_TRUTH_5MM=dir_npz + "OptimisedGeometry_BP5mm_4e9protons_withTimestamps_S1AX_lookup.npz",
+                            f_reg_ee=1.0,
+                            f_reg_ep=1.0,
+                            f_reg_xe=1.0,
+                            f_reg_ye=1.0,
+                            f_reg_ze=1.0,
+                            f_reg_xp=1.0,
+                            f_reg_yp=1.0,
+                            f_reg_zp=1.0,
+                            f_eff=1.0,
+                            f_pur=1.0,
+                            mod_bg=False)
+ToyGenerator.generate_control_plots(FILE_NAME="S1AX_continuous_master",
+                                    TAG="base",
+                                    PATH_NN_PRED=dir_toy + "S1AX_continuous_master" + "/" + "S1AX_continuous_master" + "_" + "base" + "_toy.npz",
+                                    PATH_MC_TRUTH_0MM=dir_npz + "OptimisedGeometry_BP0mm_2e10protons_withTimestamps_S1AX_lookup.npz",
+                                    PATH_MC_TRUTH_5MM=dir_npz + "OptimisedGeometry_BP5mm_4e9protons_withTimestamps_S1AX_lookup.npz")
+
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Automation scripts
+
+
+ToyGenerator.create_toy_set(FILE_NAME="S1AX_continuous_master",
+                            TAG="ee09ep09",
+                            PATH_NN_PRED_0MM=dir_results + "DNN_S1AX_continuous_master/OptimisedGeometry_BP0mm_2e10protons_withTimestamps_DNN_S1AX/OptimisedGeometry_BP0mm_2e10protons_withTimestamps_DNN_S1AX.npz",
+                            PATH_NN_PRED_5MM=dir_results + "DNN_S1AX_continuous_master/OptimisedGeometry_BP5mm_4e9protons_withTimestamps_DNN_S1AX/OptimisedGeometry_BP5mm_4e9protons_withTimestamps_DNN_S1AX.npz",
+                            PATH_MC_TRUTH_0MM=dir_npz + "OptimisedGeometry_BP0mm_2e10protons_withTimestamps_S1AX_lookup.npz",
+                            PATH_MC_TRUTH_5MM=dir_npz + "OptimisedGeometry_BP5mm_4e9protons_withTimestamps_S1AX_lookup.npz",
+                            f_reg_ee=0.9,
+                            f_reg_ep=0.9,
+                            f_reg_xe=1.0,
+                            f_reg_ye=1.0,
+                            f_reg_ze=1.0,
+                            f_reg_xp=1.0,
+                            f_reg_yp=1.0,
+                            f_reg_zp=1.0,
+                            f_eff=1.0,
+                            f_pur=1.0,
+                            mod_bg=False)
