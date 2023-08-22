dfa7e00e443ab875946a773b1cb230685cab9b8b
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-08-22 15:49:01 +0200

    added GATConv model with ResNet architecture

---
+++ analysis/GATConvResNetCluster/dataset.py
@@ -0,0 +1,176 @@
+import os
+import numpy as np
+import spektral
+import pandas as pd
+
+from spektral.data import Dataset, Graph
+from spektral.utils import io, sparse
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+################################################################################
+#
+################################################################################
+
+class GraphCluster(Dataset):
+    def __init__(self,
+                 name,
+                 adj_arg="Binary",
+                 norm_x=None,
+                 norm_e=None,
+                 p_only=False,
+                 reg_type=None,
+                 **kwargs):
+
+        self.name = name
+        self.adj_arg = adj_arg
+        self.p_only = p_only
+        self.reg_type = reg_type
+
+        self.norm_x = norm_x
+        self.norm_e = norm_e
+
+        super().__init__(**kwargs)
+
+    @property
+    def path(self):
+        # get current path, go two subdirectories higher
+        path = os.getcwd()
+        while True:
+            if os.path.basename(path) == "SiFiCC-SplitNeuralNetwork":
+                break
+            path = os.path.abspath(os.path.join(path, os.pardir))
+        path = os.path.join(path, "datasets", "SiFiCCNN_GraphCluster", self.name)
+
+        return path
+
+    def download(self):
+        print("Dunno some download function")
+
+    def read(self):
+
+        # Batch index
+        node_batch_index = np.load(self.path + "/" + self.name + "_graph_indicator.npy")
+        n_nodes = np.bincount(node_batch_index)
+        n_nodes_cum = np.concatenate(([0], np.cumsum(n_nodes)[:-1]))
+
+        # Read edge lists
+        edges = np.load(self.path + "/" + self.name + "_A.npy")
+
+        # Split edges into separate edge lists
+        edge_batch_idx = node_batch_index[edges[:, 0]]
+        n_edges = np.bincount(edge_batch_idx)
+        n_edges_cum = np.cumsum(n_edges[:-1])
+        el_list = np.split(edges - n_nodes_cum[edge_batch_idx, None],
+                           n_edges_cum)
+
+        # get node attributes (x_list)
+        x_list = self._get_x_list(n_nodes_cum=n_nodes_cum)
+        # get edge attributes (e_list)
+        e_list = self._get_e_list(n_edges_cum=n_edges_cum)
+
+        # Create sparse adjacency matrices and re-sort edge attributes in lexicographic order
+        a_e_list = [sparse.edge_index_to_matrix(edge_index=el,
+                                                edge_weight=np.ones(el.shape[0]),
+                                                edge_features=e,
+                                                shape=(n, n), )
+                    for el, e, n in zip(el_list, e_list, n_nodes)
+                    ]
+        a_list, e_list = list(zip(*a_e_list))
+
+        # set dataset target (classification / regression)
+        y_list = self._get_y_list()
+        labels = np.load(self.path + "/" + self.name + "_graph_labels.npy")
+
+        # limited to True positives only if needed
+        if self.p_only:
+            # Convert to Graph
+            print("Successfully loaded {}.".format(self.name))
+            return [
+                Graph(x=x, a=a, e=e, y=y)
+                for x, a, e, y, label in zip(x_list, a_list, e_list, y_list, labels) if label
+            ]
+
+        # Convert to Graph
+        print("Successfully loaded {}.".format(self.name))
+        return [
+            Graph(x=x, a=a, e=e, y=y)
+            for x, a, e, y in zip(x_list, a_list, e_list, labels)
+        ]
+
+    def _get_x_list(self, n_nodes_cum):
+        # Node features
+        x_attr = np.load(self.path + "/" + self.name + "_node_attributes.npy")
+        if self.norm_x is None:
+            self.norm_x = self._get_standardization(x_attr)
+        self._standardize(x_attr, self.norm_x)
+        x_list = np.split(x_attr, n_nodes_cum[1:])
+
+        return x_list
+
+    def _get_e_list(self, n_edges_cum):
+        e_attr = np.load(self.path + "/" + self.name + "_edge_attributes.npy")  # ["arr_0"]
+        if self.norm_e is None:
+            self.norm_e = self._get_standardization(e_attr)
+        self._standardize(e_attr, self.norm_e)
+        e_list = np.split(e_attr, n_edges_cum)
+        return e_list
+
+    def _get_y_list(self):
+        if self.reg_type is not None:
+            graph_attributes = np.load(self.path + "/" + self.name + "_graph_attributes.npy")
+            if self.reg_type == "Energy":
+                y_list = graph_attributes[:, :2]
+            elif self.reg_type == "Position":
+                y_list = graph_attributes[:, 2:]
+            else:
+                print("Warning: Regression type not set correctly")
+                return None
+
+        else:
+            # return class labels
+            y_list = np.load(self.path + "/" + self.name + "_graph_labels.npy")
+        return y_list
+
+    def get_classweight_dict(self):
+        labels = np.load(self.path + "/" + self.name + "_graph_labels.npy")  # ["arr_0"]
+
+        _, counts = np.unique(labels, return_counts=True)
+        class_weights = {0: len(labels) / (2 * counts[0]),
+                         1: len(labels) / (2 * counts[1])}
+
+        return class_weights
+
+    @staticmethod
+    def _get_standardization(x):
+        """
+        Returns array of mean and std of features along the -1 axis
+
+        Args:
+            x (numpy array): feature matrix
+
+        Returns:
+            ary_mean, ary_std
+        """
+
+        ary_norm = np.zeros(shape=(x.shape[1], 2))
+        ary_norm[:, 0] = np.mean(x, axis=0)
+        ary_norm[:, 1] = np.std(x, axis=0)
+
+        return ary_norm
+
+    @staticmethod
+    def _standardize(x, ary_norm):
+        for i in range(x.shape[1]):
+            x[:, i] -= ary_norm[i, 0]
+            x[:, i] /= ary_norm[i, 1]
+
+    @property
+    def sp(self):
+        sp = np.load(self.path + "/" + self.name + "_graph_sp.npy")
+        return sp
+
+    @property
+    def pe(self):
+        pe = np.load(self.path + "/" + self.name + "_graph_pe.npy")
+        return pe
---
+++ analysis/GATConvResNetCluster/downloader.py
@@ -0,0 +1,130 @@
+import numpy as np
+import os
+
+
+def load(RootParser,
+         path="",
+         n=None):
+    """
+    Script to generate a dataset in graph basis.
+
+    Inspired by the TUdataset "PROTEIN"
+
+    Two iterations over the root file are needed: one to determine the array size, one to read the
+    data. Final data is stored as npy files, separated by their usage.
+
+    Args:
+        RootParser  (root Object): root object containing root file
+        path        (str): destination path, if not given it will default to
+                           scratch_g4rt1
+        n           (int or None): Number of events sampled from root file
+
+    Return:
+         None
+    """
+
+    # define dataset name
+    dataset_name = "GraphCluster"
+    dataset_name += "_" + RootParser.file_name
+
+    # grab correct filepath, generate dataset in target directory.
+    if path == "":
+        path = "/net/scratch_g4rt1/fenger/datasets/"
+    path = os.path.join(path, "SiFiCCNN_GraphCluster", dataset_name)
+    if not os.path.isdir(path):
+        os.makedirs(path, exist_ok=True)
+
+    # Pre-determine the final array size.
+    # Total number of graphs is needed (n samples)
+    # Total number of nodes (Iteration over root file needed)
+    print("Counting number of graphs to be created")
+    if n is None:
+        n_graphs = RootParser.events_entries
+    else:
+        n_graphs = n
+    n_nodes = 0
+    m_edges = 0
+    for i, event in enumerate(RootParser.iterate_events(n=n_graphs)):
+        n_nodes += len(event.RecoClusterEntries)
+        m_edges += len(event.RecoClusterEntries) * len(event.RecoClusterEntries)
+    print("Number of Graphs to be created: ", n_graphs)
+    print("Total number of nodes to be created: ", n_nodes)
+    print("Graph features: ", 10)
+    print("Graph features: ", 3)
+
+    # creating final arrays
+    # datatypes are chosen for minimal size possible (duh)
+    ary_A = np.zeros(shape=(m_edges, 2), dtype=np.int)
+    ary_graph_indicator = np.zeros(shape=(n_nodes,), dtype=np.int)
+    ary_graph_labels = np.zeros(shape=(n_graphs,), dtype=np.bool)
+    ary_node_attributes = np.zeros(shape=(n_nodes, 10), dtype=np.float32)
+    ary_graph_attributes = np.zeros(shape=(n_graphs, 8), dtype=np.float32)
+    ary_edge_attributes = np.zeros(shape=(m_edges, 6), dtype=np.float32)
+    # meta data
+    ary_ep = np.zeros(shape=(n_graphs,), dtype=np.float32)
+    ary_sp = np.zeros(shape=(n_graphs,), dtype=np.float32)
+
+    node_id = 0
+    edge_id = 0
+    for i, event in enumerate(RootParser.iterate_events(n=n_graphs)):
+        # get number of cluster
+        n_cluster = int(len(event.RecoClusterEntries))
+        # idx_scat, idx_abs = event.sort_clusters_by_module()
+        for j in range(n_cluster):
+            for k in range(n_cluster):
+                """
+                if j in idx_abs and k in idx_scat:
+                    continue
+                """
+                ary_A[edge_id, :] = [node_id, node_id - j + k]
+
+                # exception for self loops
+                if j != k:
+                    # grab edge features in polar and cartesian representation
+                    r, phi, theta = event.get_edge_features(j, k, cartesian=False)
+                    dx, dy, dz = event.get_edge_features(j, k, cartesian=True)
+                else:
+                    r, phi, theta = 0, 0, 0
+                    dx, dy, dz = 0, 0, 0
+
+                ary_edge_attributes[edge_id, :] = [dx, dy, dz, r, phi, theta]
+                edge_id += 1
+
+            ary_graph_indicator[node_id] = i
+            ary_node_attributes[node_id, :] = [event.RecoClusterEntries[j],
+                                               event.RecoClusterTimestamps_relative[j],
+                                               event.RecoClusterEnergies_values[j],
+                                               event.RecoClusterEnergies_uncertainty[j],
+                                               event.RecoClusterPosition.x[j],
+                                               event.RecoClusterPosition.y[j],
+                                               event.RecoClusterPosition.z[j],
+                                               event.RecoClusterPosition_uncertainty.x[j],
+                                               event.RecoClusterPosition_uncertainty.y[j],
+                                               event.RecoClusterPosition_uncertainty.z[j]]
+            node_id += 1
+
+        # grab target labels and attributes
+        distcompton_tag = event.get_distcompton_tag()
+        target_energy_e, target_energy_p = event.get_target_energy()
+        target_position_e, target_position_p = event.get_target_position()
+        ary_graph_labels[i] = distcompton_tag * 1
+        ary_graph_attributes[i, :] = [target_energy_e,
+                                      target_energy_p,
+                                      target_position_e.x,
+                                      target_position_e.y,
+                                      target_position_e.z,
+                                      target_position_p.x,
+                                      target_position_p.y,
+                                      target_position_p.z]
+
+        ary_ep[i] = event.MCEnergy_Primary
+        ary_sp[i] = event.MCPosition_source.z
+
+    np.save(path + "/" + dataset_name + "_A.npy", ary_A)
+    np.save(path + "/" + dataset_name + "_graph_indicator.npy", ary_graph_indicator)
+    np.save(path + "/" + dataset_name + "_graph_labels.npy", ary_graph_labels)
+    np.save(path + "/" + dataset_name + "_node_attributes.npy", ary_node_attributes)
+    np.save(path + "/" + dataset_name + "_graph_attributes.npy", ary_graph_attributes)
+    np.save(path + "/" + dataset_name + "_edge_attributes.npy", ary_edge_attributes)
+    np.save(path + "/" + dataset_name + "_graph_pe.npy", ary_ep)
+    np.save(path + "/" + dataset_name + "_graph_sp.npy", ary_sp)
