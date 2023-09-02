fbf2edab5d329cbea91a62b45f692718cb572cbd
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-09-02 14:07:38 +0200

    Update script_generate_datasets.py

--- scripts/script_generate_datasets.py
+++ scripts/script_generate_datasets.py
@@ -20,7 +20,7 @@ path_root = path + "/root_files/"
 path_datasets = path + "/datasets/"
 
 ####################################################################################################
-
+"""
 from analysis.DNNCluster import downloader
 root_parser_0mm = RootParser.RootParser(path_root + RootFiles.onetoone_BP0mm_taggingv2)
 root_parser_5mm = RootParser.RootParser(path_root + RootFiles.onetoone_BP5mm_taggingv2)
@@ -28,3 +28,15 @@ root_parser_cont = RootParser.RootParser(path_root + RootFiles.onetoone_cont_tag
 downloader.load(root_parser_0mm, path=path_datasets, n=None)
 downloader.load(root_parser_5mm, path=path_datasets, n=None)
 downloader.load(root_parser_cont, path=path_datasets, n=None)
+"""
+
+from analysis.EdgeConvResNetCluster import downloader
+
+root_parser_0mm = RootParser.RootParser(path_root + RootFiles.onetoone_BP0mm_simV2)
+root_parser_5mm = RootParser.RootParser(path_root + RootFiles.onetoone_BP5mm_simV2)
+root_parser_cont = RootParser.RootParser(path_root + RootFiles.onetoone_CONT_simV2)
+root_parser_m5mm = RootParser.RootParser(path_root + RootFiles.onetoone_BPminus5mm_simV2)
+downloader.load(root_parser_0mm, path=path_datasets, n=None)
+downloader.load(root_parser_5mm, path=path_datasets, n=None)
+downloader.load(root_parser_cont, path=path_datasets, n=None)
+downloader.load(root_parser_m5mm, path=path_datasets, n=None)
