55a87744acd977c8e830bc2f95cd6aea5614d5f3
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-09-10 22:49:25 +0200

    Update FullAnalysisChain.py

--- analysis/EdgeConvResNetCluster/FullAnalysisChain.py
+++ analysis/EdgeConvResNetCluster/FullAnalysisChain.py
@@ -74,17 +74,22 @@ def main():
                                                                    "ReZero": ReZero})
     print("Position Regression model loaded!")
 
-    for file in [DATASET_CONT, DATASET_0MM, DATASET_5MM, DATASET_m5MM]:
+    for file in [DATASET_0MM]:
         # gather all network predictions
         y_score_pred = get_model_pred(file, path_datasets, classifier_model, norm_x, norm_e, None)
+        y_score_true = get_model_true(file, path_datasets, classifier_model, norm_x, norm_e, None)
         y_regE_pred = get_model_pred(file, path_datasets, regEnergy_model, norm_x, norm_e,
                                      "Energy")
         y_regP_pred = get_model_pred(file, path_datasets, regPosition_model, norm_x, norm_e,
                                      "Position")
         # define positive classified events
-        idx_pos = y_score_pred > threshold
+        idx_pos = np.zeros(shape=(len(y_score_pred,)))
+        for i in range(len(idx_pos)):
+            if y_score_true[i] == 0 and y_score_pred[i] > threshold:
+                idx_pos[i] = 1
+        idx_pos = idx_pos == 1
 
-        os.chdir(path + file + "/")
+        os.chdir(path_results + file + "/")
 
         # export to root file compatible with CC6 image reconstruction
         IRExport.export_CC6(ary_e=y_regE_pred[idx_pos, 0],
@@ -95,7 +100,7 @@ def main():
                             ary_px=y_regP_pred[idx_pos, 3],
                             ary_py=y_regP_pred[idx_pos, 4],
                             ary_pz=y_regP_pred[idx_pos, 5],
-                            filename="CC6IR_{}_{}".format(RUN_CODE, file),
+                            filename="CC6IR_FPONLY_{}_{}".format(RUN_CODE, file),
                             verbose=1,
                             veto=True)
 
@@ -126,9 +131,45 @@ def get_model_pred(name_dataset,
     y_pred = np.vstack(y_pred)
     if reg_type is None:
         y_pred = np.reshape(y_pred, newshape=(y_pred.shape[0],))
-    if reg_type is "Energy":
+    if reg_type == "Energy":
         y_pred = np.reshape(y_pred, newshape=(y_pred.shape[0], 2))
-    if reg_type is "Position":
+    if reg_type == "Position":
         y_pred = np.reshape(y_pred, newshape=(y_pred.shape[0], 6))
 
     return y_pred
+
+def get_model_true(name_dataset,
+                   path,
+                   classifier_model,
+                   norm_x,
+                   norm_e,
+                   reg_type=None):
+    # load dataset
+    data = dataset.GraphCluster(name=name_dataset,
+                                edge_atr=True,
+                                adj_arg="binary",
+                                norm_x=norm_x,
+                                norm_e=norm_e,
+                                reg_type=reg_type)
+    loader_test = DisjointLoader(data,
+                                 batch_size=64,
+                                 epochs=1,
+                                 shuffle=False)
+
+    y_true = []
+    for batch in loader_test:
+        inputs, target = batch
+        y_true.append(target)
+    y_true = np.vstack(y_true)
+    if reg_type is None:
+        y_true = np.reshape(y_true, newshape=(y_true.shape[0],))
+    if reg_type == "Energy":
+        y_true = np.reshape(y_true, newshape=(y_true.shape[0], 2))
+    if reg_type == "Position":
+        y_true = np.reshape(y_true, newshape=(y_true.shape[0], 6))
+
+    return y_true
+
+
+if __name__ == "__main__":
+    main()
