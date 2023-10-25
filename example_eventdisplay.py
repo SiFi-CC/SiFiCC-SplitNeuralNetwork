217717f4a960070c9c0341a7bfb483665e880ff3
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-10-25 22:25:04 +0200

    Update example_eventdisplay.py

--- example_eventdisplay.py
+++ example_eventdisplay.py
@@ -2,38 +2,38 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from src.SiFiCCNN.EventDisplay import EDDisplay
-from src.SiFiCCNN.Root import RootParser, RootFiles, RootLogger
+from SiFiCCNN.EventDisplay import EventDisplay
+from SiFiCCNN.root import RootParser, RootFiles
 
 dir_main = os.getcwd()
+dir_root = dir_main + "/root_files/"
 
-# ----------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+"""
+root_parser = RootParser.RootParser(dir_root + RootFiles.onetoone_BP0mm_taggingv2)
+for i, event in enumerate(root_parser.iterate_events(n=None)):
 
-root_parser = RootParser.Root(dir_main + RootFiles.OptimisedGeometry_BP5mm_4e9protons_withTimestamps_local)
-print(root_parser.events_keys)
-print(root_parser.ifglobal)
-print(root_parser.ifreco)
-print(root_parser.ifcluster)
-print(root_parser.ifsipm)
-
-for i in range(1000):
-    event = root_parser.get_event(i)
-
-    if event.compton_tag:
+    if not event.compton_tag:
         continue
 
-    # feature_map = event.get_sipm_feature_map()
-    RootLogger.print_event_summary(root_parser, i)
+    event.summary()
     EDDisplay.display(event)
+"""
 
-    """
-    fig1, axs = plt.subplots(1, 2)
-    axs[0].imshow(feature_map[:, 0 + 2, :, 0])
-    axs[1].imshow(feature_map[:, 1 + 2, :, 0])
-    plt.show()
 
-    fig2, axs = plt.subplots(1, 2)
-    axs[0].imshow(feature_map[:, 0 + 2, :, 1])
-    axs[1].imshow(feature_map[:, 1 + 2, :, 1])
-    plt.show()
-    """
+# --------------------------------------------------------------------------------------------------
+
+root_parser = RootParser.RootParser(dir_root + RootFiles.fourtoone_BP0mm_test)
+test_event = root_parser.get_event(2)
+
+display = EventDisplay.EventDisplay()
+display.load_event(test_event)
+display.draw_detector()
+display.draw_reference_axis()
+display.draw_promptgamma()
+display.draw_interactions()
+display.draw_cone_true()
+display.draw_fibre_hits()
+display.draw_sipm_hits()
+display.show()
+
