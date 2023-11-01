ea6fab23677c8387212acaa6775193747d309fca
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-11-01 19:33:03 +0100

    added comments, added exceptions for corrdinate systems

--- analysis/EdgeConvResNetSiPM/downloader.py
+++ analysis/EdgeConvResNetSiPM/downloader.py
@@ -1,41 +1,51 @@
+####################################################################################################
+# ### EdgeConvResNetSiPM downloader.py
+#
+# This is an example downloader script for the EdgeConvResNetSiPM configuration, a dynamic graph
+# structure. Exceptions are implemented to accept different coordinate systems. The final output
+# will be in the form of the "Aachen" coordinate system.
+#
+####################################################################################################
+
 import numpy as np
 import os
 
 
 def load(RootParser,
          path="",
-         n=None):
+         n=None,
+         coordinate_system="CRACOW"):
     """
-    Script to generate a dataset in graph basis.
-
-    Inspired by the TUdataset "PROTEIN"
+    Script to generate a dataset in graph basis. Inspired by the TUdataset "PROTEIN"
 
     Two iterations over the root file are needed: one to determine the array size, one to read the
     data. Final data is stored as npy files, separated by their usage.
 
     Args:
-        RootParser  (root Object): root object containing root file
-        path        (str): destination path, if not given it will default to
-                           scratch_g4rt1
-        n           (int or None): Number of events sampled from root file
+        RootParser  (root Object):  root object containing root file
+        path        (str):          destination path, if not given it will default to
+                                    scratch_g4rt1
+        n           (int or None):  Number of events sampled from root file
+        coordinate_system (str):    Coordinate system of the given root file, everything will be
+                                    converted to Aachen coordinate system
 
     Return:
          None
     """
 
-    # define dataset name
-    dataset_name = "GraphSiPM"
-    dataset_name += "_" + RootParser.file_name
+    # define dataset name, constructed from the given input and the final data structure
+    # grab correct filepath, generate dataset in target directory
+    base = "GraphSiPM"
+    name_set = "{}_{}".format(base, RootParser.file_name)
 
-    # grab correct filepath, generate dataset in target directory.
     if path == "":
         path = "/net/scratch_g4rt1/fenger/datasets/"
-    path = os.path.join(path, "SiFiCCNN_GraphSiPM", dataset_name)
+    path = os.path.join(path, "SiFiCCNN_{}".format(base), name_set)
     if not os.path.isdir(path):
         os.makedirs(path, exist_ok=True)
 
     # Pre-determine the final array size.
-    # Total number of graphs is needed (n samples)
+    # Total number of graphs needed (n samples)
     # Total number of nodes (Iteration over root file needed)
     print("Counting number of graphs to be created")
     if n is None:
@@ -64,6 +74,8 @@ def load(RootParser,
     ary_pe = np.zeros(shape=(k_graphs,), dtype=np.float32)
     ary_sp = np.zeros(shape=(k_graphs,), dtype=np.float32)
 
+    # main iteration over root file, containing beta coincidence check
+    # TODO: Coincidence check
     graph_id = 0
     node_id = 0
     edge_id = 0
@@ -81,12 +93,22 @@ def load(RootParser,
                 edge_id += 1
 
             ary_graph_indicator[node_id] = graph_id
-            ary_node_attributes[node_id, :] = [event.SiPM_position.x[j],
-                                               event.SiPM_position.y[j],
-                                               event.SiPM_position.z[j],
-                                               event.SiPM_triggertime[j],
-                                               event.SiPM_qdc[j]]
 
+            # exception for different coordinate systems
+            if coordinate_system == "CRACOW":
+                ary_node_attributes[node_id, :] = [event.SiPM_position.z[j],
+                                                   -event.SiPM_position.y[j],
+                                                   event.SiPM_position.x[j],
+                                                   event.SiPM_timestamp[j],
+                                                   event.SiPM_photoncount[j]]
+            elif coordinate_system == "AACHEN":
+                ary_node_attributes[node_id, :] = [event.SiPM_position.x[j],
+                                                   event.SiPM_position.y[j],
+                                                   event.SiPM_position.z[j],
+                                                   event.SiPM_timestamp[j],
+                                                   event.SiPM_photoncount[j]]
+
+            # counter
             node_id += 1
 
         # grab target labels and attributes
@@ -94,23 +116,36 @@ def load(RootParser,
         target_energy_e, target_energy_p = event.get_target_energy()
         target_position_e, target_position_p = event.get_target_position()
         ary_graph_labels[graph_id] = distcompton_tag
-        ary_graph_attributes[graph_id, :] = [target_energy_e,
-                                             target_energy_p,
-                                             target_position_e.x,
-                                             target_position_e.y,
-                                             target_position_e.z,
-                                             target_position_p.x,
-                                             target_position_p.y,
-                                             target_position_p.z]
-
         ary_pe[graph_id] = event.MCEnergy_Primary
-        ary_sp[graph_id] = event.MCPosition_source.z
+        # exception for different coordinate systems
+        if coordinate_system == "CRACOW":
+            ary_graph_attributes[graph_id, :] = [target_energy_e,
+                                                 target_energy_p,
+                                                 target_position_e.z,
+                                                 -target_position_e.y,
+                                                 target_position_e.x,
+                                                 target_position_p.z,
+                                                 -target_position_p.y,
+                                                 target_position_p.x]
+            ary_sp[graph_id] = event.MCPosition_source.z
+        elif coordinate_system == "AACHEN":
+            ary_graph_attributes[graph_id, :] = [target_energy_e,
+                                                 target_energy_p,
+                                                 target_position_e.x,
+                                                 target_position_e.y,
+                                                 target_position_e.z,
+                                                 target_position_p.x,
+                                                 target_position_p.y,
+                                                 target_position_p.z]
+            ary_sp[graph_id] = event.MCPosition_source.x
+
+        # counter
         graph_id += 1
 
-    np.save(path + "/" + dataset_name + "_A.npy", ary_A)
-    np.save(path + "/" + dataset_name + "_graph_indicator.npy", ary_graph_indicator)
-    np.save(path + "/" + dataset_name + "_graph_labels.npy", ary_graph_labels)
-    np.save(path + "/" + dataset_name + "_node_attributes.npy", ary_node_attributes)
-    np.save(path + "/" + dataset_name + "_graph_attributes.npy", ary_graph_attributes)
-    np.save(path + "/" + dataset_name + "_graph_pe.npy", ary_pe)
-    np.save(path + "/" + dataset_name + "_graph_sp.npy", ary_sp)
+    np.save(path + "/" + name_set + "_A.npy", ary_A)
+    np.save(path + "/" + name_set + "_graph_indicator.npy", ary_graph_indicator)
+    np.save(path + "/" + name_set + "_graph_labels.npy", ary_graph_labels)
+    np.save(path + "/" + name_set + "_node_attributes.npy", ary_node_attributes)
+    np.save(path + "/" + name_set + "_graph_attributes.npy", ary_graph_attributes)
+    np.save(path + "/" + name_set + "_graph_pe.npy", ary_pe)
+    np.save(path + "/" + name_set + "_graph_sp.npy", ary_sp)
