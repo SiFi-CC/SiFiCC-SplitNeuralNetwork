7d4d2314850b8634d39c68824fb5c9e088bc45a3
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2023-11-24 10:16:44 +0100

    removed redundant code, updated ResNet block structure

--- SiFiCCNN/utils/layers.py
+++ SiFiCCNN/utils/layers.py
@@ -128,55 +128,6 @@ def GCNConvResNetBlock(x,
     return x
 
 
-def ECCConvResNetBlock(x,
-                       A,
-                       E,
-                       n_filter=64,
-                       activation="relu",
-                       conv_activation="relu",
-                       kernel_initializer="glorot_uniform"):
-    r"""
-    ResNetBlock implementation used in the master thesis
-    'Vertex Reconstruction for Neutrino Events in the Double Chooz Experiment using Graph Neural
-    Networks'
-    Utilizes graph convolutions (Kipf & Welling) instead of conventional layers.Conv...
-    Instead of the standard short cut the ReZero approach is used.
-
-    Args:
-        x: A keras layers object which is compatible with
-           sp.layers.GCNConv with output shape [batch, nodes, features]
-        A: Adjacency matrix (Modified Laplacian of shape [batch, nodes, nodes])
-           e.g. in form of an Input layer:
-           A = tf.keras.layers.Input(tensor = sp.layers.ops.sp_matrix_to_sp_tensor(sp.utils.gcn_filter(Adj)))
-        n_filter: number of convolution filters
-        conv_activation: activation function of the first convolution layer in the block
-        kernel_initializer: initializer for the weights
-        activation: activation function which is applied after adding identity to conv block
-    Returns:
-        A keras layer. Graph after GCNConvResNetBlock with shape [batch, n_nodes, n_filter]
-    """
-
-    # two convolution layers in ResNetBlock
-    fx = sp.layers.ECCConv(n_filter,
-                           activation=conv_activation,
-                           use_bias=True,
-                           kernel_initializer=kernel_initializer)([x, A, E])
-    fx = sp.layers.ECCConv(n_filter,
-                           use_bias=False,
-                           kernel_initializer=kernel_initializer)([fx, A, E])
-
-    # adjust the number of filters in the short cut path by eventually applying a Conv1D with kernel size 1
-    x = adjustChannelSize(x, fx)
-
-    # apply rezero layer
-    x = ReZero()([x, fx])
-
-    # apply activation function at the end
-    x = tf.keras.layers.Activation(activation)(x)
-
-    return x
-
-
 def EdgeConvResNetBlock(x,
                         A,
                         n_filter=64,
@@ -225,18 +176,15 @@ def EdgeConvResNetBlock(x,
     return x
 
 
-def GATConvResNetBlock(x,
-                       A,
-                       n_filter=64,
-                       attn_heads=8,
-                       activation="relu",
-                       conv_activation="relu",
-                       kernel_initializer="glorot_uniform"):
-    r"""
-    ResNetBlock implementation used in the master thesis
-    'Vertex Reconstruction for Neutrino Events in the Double Chooz Experiment using Graph Neural
-    Networks'
-    Utilizes graph convolutions (Kipf & Welling) instead of conventional layers.Conv...
+def EdgeConvResNetBlockV2(x,
+                          A,
+                          n_filter=64,
+                          activation="relu",
+                          kernel_initializer="glorot_uniform"):
+    """
+    Updated ResNetBlock implementation from https://arxiv.org/pdf/1603.05027v3.pdf
+    Compared to the standard ResNet implementation the order of BN and ReLU is changed for better
+    identify mapping. Currently, Batch Normalization is not included!
     Instead of the standard short cut the ReZero approach is used.
 
     Args:
@@ -246,34 +194,29 @@ def GATConvResNetBlock(x,
            e.g. in form of an Input layer:
            A = tf.keras.layers.Input(tensor = sp.layers.ops.sp_matrix_to_sp_tensor(sp.utils.gcn_filter(Adj)))
         n_filter: number of convolution filters
-        conv_activation: activation function of the first convolution layer in the block
         kernel_initializer: initializer for the weights
         activation: activation function which is applied after adding identity to conv block
     Returns:
         A keras layer. Graph after GCNConvResNetBlock with shape [batch, n_nodes, n_filter]
     """
 
-    # two convolution layers in ResNetBlock
-    fx = sp.layers.GATConv(n_filter,
-                           attn_heads=attn_heads,
-                           concat_heads=True,
-                           use_bias=True,
-                           kernel_initializer=kernel_initializer)([x, A])
-    fx = sp.layers.GATConv(n_filter,
-                           attn_heads=attn_heads,
-                           concat_heads=True,
-                           use_bias=False,
-                           kernel_initializer=kernel_initializer)([fx, A])
+    # two weight layers in ResNetBlock
+    #fx = tf.keras.layers.BatchNormalization(axis=0)(x)
+    fx = tf.keras.layers.Activation(activation)(x)
+    fx = sp.layers.EdgeConv(channels=n_filter,
+                            use_bias=True,
+                            kernel_initializer=kernel_initializer)([fx, A])
+    #fx = tf.keras.layers.BatchNormalization(axis=0)(fx)
+    fx = tf.keras.layers.Activation(activation)(fx)
+    fx = sp.layers.EdgeConv(channels=n_filter,
+                            use_bias=False,
+                            kernel_initializer=kernel_initializer)([fx, A])
 
     # adjust the number of filters in the short cut path by eventually applying a Conv1D with kernel size 1
     x = adjustChannelSize(x, fx)
 
     # apply rezero layer
     x = ReZero()([x, fx])
-
-    # apply activation function at the end
-    x = tf.keras.layers.Activation(activation)(x)
-
     return x
 
 
